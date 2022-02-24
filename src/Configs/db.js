68c00031d3fa236b6074dcbed4e5650182ab163a
kamal <kamaldro97@gmail.com>
2022-02-24 19:12:19 +0530

    cleaning fs

--- src/Configs/db.js
+++ src/Configs/db.js
@@ -2,5 +2,7 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 module.exports = () => {
-  return mongoose.connect(process.env.ATLAS_DB_SECRET_KEY);
+  return mongoose.connect(
+    `mongodb+srv://kamal:${process.env.ATLAS_DB_SECRET_KEY}@awscluster.ummcp.mongodb.net/sdetask?retryWrites=true&w=majority`
+  );
 };
