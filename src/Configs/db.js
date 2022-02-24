6201d4295197fe0c4586b54e941115267d189871
kamal <kamaldro97@gmail.com>
2022-02-24 11:32:59 +0530

    db changed to atlas

--- src/Configs/db.js
+++ src/Configs/db.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 
 module.exports = () => {
-  return mongoose.connect("mongodb://localhost:27017/sdetask");
+  return mongoose.connect(
+    "mongodb+srv://kamal:kamal@awscluster.ummcp.mongodb.net/sdetask?retryWrites=true&w=majority"
+  );
+  // return mongoose.connect("mongodb://localhost:27017/sdetask");
 };
